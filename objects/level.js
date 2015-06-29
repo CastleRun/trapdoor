c7173481853762e738698643de1d113c4aa1c5d5
CastleRun <786901a@gmail.com>
2015-06-29 07:43:57 -0700

    Update level.js

--- objects/level.js
+++ objects/level.js
@@ -1,6 +1,6 @@
 var Level={
  door:undefined
  key:undefined
-setup:function(dx,dy,kx,ky){door=trapdoor;key=Key;door.keyid=0;key.keyid=0;door.pos=Position(dx,dy);key.pos=Position(kx,ky);},
- check:function(){if(door.open){alert("You win!");}}
+stup:function(dx,dy,kx,ky){door=trapdoor;key=Key;door.keyid=0;key.keyid=0;door.pos=Position(dx,dy);key.pos=Position(kx,ky);},
+ chek:function(){if(door.open){alert("You win!");}}
 }
