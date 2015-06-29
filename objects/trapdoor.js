f306bfd1bf86f69861a73207ded5dc9a638b5438
CastleRun <786901a@gmail.com>
2015-06-29 07:26:59 -0700

    Update trapdoor.js

--- objects/trapdoor.js
+++ objects/trapdoor.js
@@ -1,6 +1,6 @@
 var trapdoor={
  open:false,
  keyid:undefined,
- open:function(key){if(key==this.keyid){this.open=true; return true;}},
+ opens:function(key){if(key==this.keyid){this.open=true; return true;}},
  pos:undefined//Construct this with the position(x,y) object constructor.
 };
